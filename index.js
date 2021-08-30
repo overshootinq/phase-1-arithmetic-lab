eb9562327fac51045256f19aaf149331afc7b55c
Brennan McCandliss <osmedia1220@gmail.com>
2021-08-30 15:29:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 8;
+const num4 = 4;
 
-// Write your code here
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() + 1);
+const mod = num3 - num4;
+const max = Math.max(5,10,15,20);
